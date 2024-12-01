ede6488f29095248512c5ad7f0c520a53db5dea6
RomanPopovski <runway963@gmail.com>
2024-12-01 13:05:51 +0100

    Add files via upload

--- js/1st-homework 2-1(lecture 22).js
+++ js/1st-homework 2-1(lecture 22).js
@@ -103,20 +103,50 @@
 
 // Привітка : все що функція списано із інтернету, як зробити сам - не зрозумів :(
 
-const digit = prompt("Введіть трьхзначне число");
-if (digit >= 100 && digit <= 999) {
-  function sumOfDigit(num) {
-    let numStr = num.toString();
-    let sum = 0;
+// const digit = prompt("Введіть трьхзначне число");
+// if (digit >= 100 && digit <= 999) {
+//   function sumOfDigit(num) {
+//     let numStr = num.toString();
+//     let sum = 0;
 
-    for (let digit of numStr) {
-      sum += parseInt(digit);
-    }
+//     for (let digit of numStr) {
+//       sum += parseInt(digit);
+//     }
 
-    return sum;
-  }
+//     return sum;
+//   }
 
-  alert(`Результат обчислень дорівнює "${sumOfDigit(digit)}"`);
-} else {
-  alert("Число має бути від 100 до 999. Оновіть сторінку та спробуйте ще раз");
-}
+//   alert(`Результат обчислень дорівнює "${sumOfDigit(digit)}"`);
+// } else {
+//   alert("Число має бути від 100 до 999. Оновіть сторінку та спробуйте ще раз");
+// }
+
+// Приклад ментора №1
+
+// const digit = prompt("Введіть тризначне число");
+// if (digit >= 100 && digit <= 999) {
+//   const num1 = parseInt(digit[0]); // Перша цифра
+//   const num2 = parseInt(digit[1]); // Друга цифра
+//   const num3 = parseInt(digit[2]); // Третя цифра
+//   const sum = num1 + num2 + num3;
+
+//   alert(`Результат обчислень дорівнює "${sum}"`);
+// } else {
+//   alert("Число має бути від 100 до 999. Спробуйте ще раз.");
+// }
+
+// Приклад ментора №2 + Моя доробка
+
+// const a = prompt("Введіть тризначне число");
+// if (digit >= 100 && digit <= 999) {
+//   function sumOfDigit(num) {
+//     const num1 = parseInt(num[0]);
+//     const num2 = parseInt(num[1]);
+//     const num3 = parseInt(num[2]);
+//     return num1 + num2 + num3;
+//   }
+//   const result = sumOfDigit(a);
+//   alert(`Результат обчислень дорівнює "${result}"`);
+// } else {
+//   alert("Число має бути від 100 до 999. Спробуйте ще раз.");
+// }
